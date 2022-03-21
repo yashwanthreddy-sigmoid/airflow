d89a93620912c1f54ed62cb0215a630d26edb3c7
yashwanthreddy-sigmoid <98736166+yashwanthreddy-sigmoid@users.noreply.github.com>
2022-03-21 10:06:00 +0530

    Update DAG_file.py

--- dags/DAG_file.py
+++ dags/DAG_file.py
@@ -15,10 +15,7 @@ default_args = {
     "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=1),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
+
 }
 
 dag = DAG("Assignment", default_args=default_args, schedule_interval="0 6 * * *")
